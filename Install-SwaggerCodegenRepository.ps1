1b30a24d32172a32fb2983206959bd9f33bfbcba
beatcracker <beatcracker@users.noreply.github.com>
2017-07-19 18:21:46 +0300

    Add SwaggerPath param Clone if no repo, pull if exists

--- Install-SwaggerCodegenRepository.ps1
+++ Install-SwaggerCodegenRepository.ps1
@@ -3,9 +3,27 @@
     Clone Swagger Codegen repository
 #>
 
-& 'git.exe' @(
+[CmdletBinding()]
+Param (
+    $SwaggerPath = '.\swagger-codegen'
+)
+
+$Clone = @(
     'clone',
+    '--quiet',
     '--depth',
     '1',
     'https://github.com/swagger-api/swagger-codegen.git'
+    $SwaggerPath
 )
+
+$Pull = @(
+    'pull',
+    '--quiet'
+)
+
+if (Test-Path -Path $SwaggerPath) {
+    & 'git.exe' $Pull
+} else {
+    & 'git.exe' $Clone
+}
